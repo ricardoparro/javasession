76f3b2631fbbb6820acb2f76fe1b38ca33454367
Ricardo Parro <ricardo.parro@gmail.com>
2014-10-12 13:48:54 -0300

    added code to reverse a c style string

---
+++ ReverseCstyleString.java
@@ -0,0 +1,30 @@
+public class ReverseCstyleString{
+    
+    public static void main(String[] args){
+	
+	char[] test = {'1','2','3','3','1','5','\0'};
+
+	char[] reversedCstring = reverseString(test);
+	
+    System.out.println(reversedCstring);
+    }
+
+    public static char[] reverseString(char[] target){
+	
+	int head = 0;
+	int tail = target.length -2;
+	
+	while(head < tail){
+	    
+	    char temp = target[head];
+	    target[head] = target[tail];
+	    target[tail] = temp;
+	    head++;
+	    tail--;
+	}
+	
+	return target;
+    }
+ 
+
+}
